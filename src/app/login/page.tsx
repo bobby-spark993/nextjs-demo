ea9fe1a4719b1f918de0ae3a37467d111ae1cabc
bobby-kumar <bobbyspaerkumar9939@gmail.com>
2026-01-17 19:04:47 +0530

    form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,7 +4,18 @@ function Login() {
       <div>
         <h1>Login</h1>
         <div>
+          <form className="flex-col justify-center items-center items-center">
+            <div>
+              <label>Email:</label>
+              <input type="text" name="email" placeholder="enter email" />
+            </div>
+            <div>
+              <label>Password</label>
+              <input type="password" name="password" placeholder="enter" />
+            </div>
 
+            <input type="submit" name="Save" />
+          </form>
         </div>
       </div>
     </div>
